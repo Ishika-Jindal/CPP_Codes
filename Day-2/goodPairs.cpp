9155f6795ebc95cbae69678b26a9efec33d8c710
Ishika-Jindal <77258832+Ishika-Jindal@users.noreply.github.com>
2021-07-01 22:40:52 +0530

    goodPairs

---
+++ Day-2/goodPairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,c=0;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[i]==arr[j] && i<j)
+				c++;
+		}
+	}
+	cout<< c;
+}
